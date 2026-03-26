b8b85baaeeedb8067e0caeb35f5625ac4dadc4dd
Dharsikask14 <skdharsika@gmail.com>
2026-03-27 00:03:57 +0530

    Added new UI changes in new-feature branch

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 console.log("JS connected successfully!");
 
+
 const button = document.getElementById("getStartedBtn");
+
 
 button.addEventListener("click", function() {
     alert("Welcome to Bloom Flower Shop 🌸");
 
+    
 });
